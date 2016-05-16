5b011d12cc7ce67285cb1f27841e8643c84931fc
Wooyoung Moon <wmoon5@gmail.com>
2016-05-16 13:03:37 -0500

    added functions

--- BDT_sklearn.py
+++ BDT_sklearn.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+import pandas as pd
 
 
 
@@ -16,6 +17,19 @@ def add_information_from_tree(tree, matrix):
             matrix[parent][right_child] += 1
     
     return matrix
+
+def get_directed_matrix(bdt):
+    connections = np.zeros((len(branch_names), len(branch_names)))
+    for tree in bdt.estimators_:
+        connections = add_information_from_tree(tree.tree_, connections)
+    return connections
+def get_undirected_matrix(bdt):
+    connections = get_directed_matrix(bdt)
+    for i in range(len(connections)):
+        for j in range(i):
+            connections[i][j] += connections[j][i]
+            connections[j][i] = 0
+    return connections
 
 
 
