eac7c842aee2353ebb756314d48537d830985dd7
Wooyoung Moon <wmoon5@gmail.com>
2016-05-18 16:21:10 -0500

    added more functions

--- BDT_sklearn.py
+++ BDT_sklearn.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-# working with data and pandas dataframes
+# specific to MITF-Myc project
 
 def OnevAll_SplitTrainTest(df, signal, TF_list):
     if len(df[df.which==signal]) > df.which.value_counts().min()*3:
@@ -31,7 +31,6 @@ def OnevAll_SplitTrainTest(df, signal, TF_list):
 
 
 
-
 #scikit-learn and BDT-specific functions
 
 def get_importances_dict(bdt, branch_names):
@@ -50,39 +49,90 @@ def order_by_importance(bdt, branch_names):
         importances_list.append(branch_names[indices[i]])
     return importances_list
 
+def find_depths_in_tree(feature_index, tree):
+    depths_in_tree = []
+    left_children = list(tree.tree_.children_left)
+    right_children = list(tree.tree_.children_right)
+
+    for i in np.where(tree.tree_.feature == feature_index)[0]:
+        index = i
+        count = 0
+        while index != 0:
+            if index in left_children:
+                index = left_children.index(index)
+                count += 1
+                #print "left"
+            elif index in right_children:
+                index = right_children.index(index)
+                count += 1
+                #print "right"
+            else:
+                print "something's wrong..."
+                index = 0
+
+        #print count
+        depths_in_tree.append(count)
+    return depths_in_tree
+
+def find_all_depths(feature_index, BDT):
+    all_depths = []
+    for tree in BDT.estimators_:
+        all_depths += find_depths_in_tree(feature_index, tree)
+    return all_depths
+
 
 
 
 ## working with Graphs
 
-def add_information_from_tree(tree, matrix):
+#def add_information_from_tree(tree, matrix, weight=1):
+#    zipped = zip(tree.children_left, tree.children_right, tree.feature, tree.threshold)
+#    for i in range(len(zipped)):
+#        if zipped[zipped[i][0]][2] < 0:
+#            pass
+#        else:
+#            parent = zipped[i][2]
+#            left_child = zipped[zipped[i][0]][2]
+#            right_child = zipped[zipped[i][1]][2]
+# 
+#            matrix[parent][left_child] += weight
+#            matrix[parent][right_child] += weight
+#    
+#    return matrix
+
+def add_information_from_tree(tree, matrix, weight=1):
     zipped = zip(tree.children_left, tree.children_right, tree.feature, tree.threshold)
     for i in range(len(zipped)):
-        if zipped[zipped[i][0]][2] < 0:
-            pass
-        else:
-            parent = zipped[i][2]
-            left_child = zipped[zipped[i][0]][2]
-            right_child = zipped[zipped[i][1]][2]
+
+        parent = zipped[i][2]
+        left_child = zipped[zipped[i][0]][2]
+        right_child = zipped[zipped[i][1]][2]
  
-            matrix[parent][left_child] += 1
-            matrix[parent][right_child] += 1
+        if left_child >= 0:
+            matrix[parent][left_child] += weight
+        if right_child >= 0:
+            matrix[parent][right_child] += weight
     
     return matrix
 
-def get_directed_matrix(bdt, branch_names):
+def get_directed_matrix(bdt, branch_names, weighted=False):
     connections = np.zeros((len(branch_names), len(branch_names)))
-    for tree in bdt.estimators_:
-        connections = add_information_from_tree(tree.tree_, connections)
+    if weighted:
+        for i in range(len(bdt.estimators_)):
+            connections = add_information_from_tree(bdt.estimators_[i].tree_, connections, weight = bdt.estimator_weights_[i])
+    else:
+        for tree in bdt.estimators_:
+            connections = add_information_from_tree(tree.tree_, connections)
     return connections
 
-def get_undirected_matrix(bdt, branch_names):
-    connections = get_directed_matrix(bdt, branch_names)
+def get_undirected_matrix(bdt, branch_names, weighted=False):
+    connections = get_directed_matrix(bdt, branch_names, weighted)
     for i in range(len(connections)):
         for j in range(i):
             connections[i][j] += connections[j][i]
             connections[j][i] = 0
     return connections
+
 
 def remove_edgeless(graph):
     degree_dict = graph.degree()
